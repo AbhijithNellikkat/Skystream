1e133388dccac5b5d39342e237906884690b930f
Abhijith Nellikkat <abhijithnellikkat@gmail.com>
2023-12-28 10:46:12 +0530

    FetchWeather function almost completed

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,13 +1,38 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   WeatherBloc() : super(WeatherInitial()) {
-    on<WeatherEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<FetchWeather>(fetchWeather);
+  }
+
+  FutureOr<void> fetchWeather(
+      FetchWeather event, Emitter<WeatherState> emit) async {
+    emit(WeatherLoading());
+    try {
+      const String apiKey = '6a03d86dffb7ce9f6398691ee68698cd';
+      WeatherFactory weatherFactory =
+          WeatherFactory(apiKey, language: Language.ENGLISH);
+
+      Weather weather = await weatherFactory.currentWeatherByLocation(
+        event.position.latitude,
+        event.position.longitude,
+      );
+
+      log('$weather');
+
+      emit(WeatherSuccess(weather: weather));
+    } catch (e) {
+      emit(WeatherFailure());
+      log('$e');
+    }
   }
 }
--- lib/bloc/weather_event.dart
+++ lib/bloc/weather_event.dart
@@ -6,3 +6,12 @@ sealed class WeatherEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class FetchWeather extends WeatherEvent {
+  final Position position;
+
+  const FetchWeather({required this.position});
+
+   @override
+  List<Object> get props => [position];
+}
